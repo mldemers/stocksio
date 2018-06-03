6060d42dfe5a295a65a0d32addbda6b62d9c14d0
mdemers <matthew.demers96@gmail.com>
2018-06-03 16:44:34 -0400

    Still not sure what im doing with my code

---
+++ src/main/resources/static/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import 'babel-core/register'
+import 'babel-polyfill'
+
+import '../styles/index.scss'
+
+ReactDOM.render(
+    console.log('Hello World'),
+    <Home />,
+    document.getElementById('app'),
+)
